6af76d4cd6ca1458c1e60c966d77b2ffa28b450f
abhijithpoojary <48614314+abhijithpoojary@users.noreply.github.com>
2021-08-24 16:13:41 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -3,8 +3,8 @@ const mailGun = require('nodemailer-mailgun-transport');
 
 const auth = {
     auth: {
-        api_key: '93f73aab054b35983fd42b00a27b50c9-9ad3eb61-5ec6267e',
-        domain: 'sandbox5e24833a512e4ff48dcae4a7a03d0744.mailgun.org'
+        api_key: '',
+        domain: ''
     }
 };
 
